3a32a2165a78e5343a22bb185e2fb919f81b0b10
David Brown <dmlb2000@gmail.com>
2016-05-20 22:41:10 -0700

    add server bits

--- recipes/server.rb
+++ recipes/server.rb
@@ -5,14 +5,35 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
 include_recipe 'x509'
+include_recipe 'lvm'
 include_recipe 'dmlb2000_users'
 directory '/etc/pki/docker'
+
+lvm_volume_group 'docker' do
+  physical_volumes ['/dev/vdb']
+  wipe_signatures true
+
+  logical_volume 'default-metadata' do
+    size        '5%VG'
+  end
+
+  logical_volume 'default-data' do
+    size        '45%VG'
+  end
+
+  logical_volume 'system-metadata' do
+    size        '5%VG'
+  end
+  logical_volume 'system-data' do
+    size        '45%VG'
+  end
+end
 
 x509_ca_certificate 'dockerCA' do
   cacertificate node['dmlb2000_docker']['certs']['ca']['pem']
 end
 
-x509_certificate 'docker.dmlb2000.org' do
+x509_certificate node['fqdn'] do
   ca 'dockerCA'
   key node['dmlb2000_docker']['certs']['server']['key']
   certificate node['dmlb2000_docker']['certs']['server']['cert']
@@ -39,6 +60,22 @@ docker_service 'default' do
   tls_server_cert node['dmlb2000_docker']['certs']['server']['cert']
   tls_server_key node['dmlb2000_docker']['certs']['server']['key']
   tls_verify node['dmlb2000_docker']['tls_verify']
+  storage_driver 'devicemapper'
+  storage_opts %w(dm.datadev=/dev/docker/default-data dm.metadatadev=/dev/docker/default-metadata)
+  group 'wheel'
+  action [:create, :start]
+end
+
+directory '/var/lib/docker-system'
+docker_service 'system' do
+  host ['unix:///var/run/docker-system.sock']
+  tls_ca_cert node['dmlb2000_docker']['certs']['ca']['pem']
+  tls_server_cert node['dmlb2000_docker']['certs']['server']['cert']
+  tls_server_key node['dmlb2000_docker']['certs']['server']['key']
+  tls_verify node['dmlb2000_docker']['tls_verify']
+  storage_driver 'devicemapper'
+  graph '/var/lib/docker-system'
+  storage_opts %w(dm.datadev=/dev/docker/system-data dm.metadatadev=/dev/docker/system-metadata)
   group 'wheel'
   action [:create, :start]
 end
