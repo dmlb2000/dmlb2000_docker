431f4a652f2e28f1116280d843c3503048286b9f
David Brown <dmlb2000@gmail.com>
2016-12-17 09:54:13 -0800

    didn't quite get this right

--- recipes/server.rb
+++ recipes/server.rb
@@ -4,7 +4,6 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
-include_recipe 'x509'
 include_recipe 'lvm'
 include_recipe 'dmlb2000_distro'
 include_recipe 'dmlb2000_chef'
@@ -29,27 +28,6 @@ lvm_volume_group 'docker' do
   logical_volume 'bootstrap-data' do
     size        '45%VG'
   end
-end
-
-x509_ca_certificate 'dockerCA' do
-  cacertificate node['dmlb2000_docker']['certs']['ca']['pem']
-end
-
-x509_certificate node['fqdn'] do
-  ca 'dockerCA'
-  key node['dmlb2000_docker']['certs']['server']['key']
-  certificate node['dmlb2000_docker']['certs']['server']['cert']
-  bits 4096
-  days 365
-end
-
-x509_certificate "dmlb2000-#{node['fqdn']}" do
-  cn node['fqdn']
-  owner 'dmlb2000'
-  ca 'dockerCA'
-  type 'client'
-  key node['dmlb2000_docker']['certs']['client']['key']
-  certificate node['dmlb2000_docker']['certs']['client']['cert']
 end
 
 package 'docker'
